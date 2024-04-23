95480c7fc93298451f023c21f79cd912fc8a4bbc
jrgcast <joao.ralph1@gmail.com>
2024-04-22 22:46:14 -0300

    Initial commit

---
+++ src/main/java/com/jrgcast/apirailway/ApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.jrgcast.apirailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrgcast/apirailway/ApiRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrgcast.apirailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
